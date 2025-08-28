ffcbcef050a88a8dbe2f6ac5cf9e45db2b05aa45
ctrl-schaff <schaffjohnathan@gmail.com>
2025-08-28 10:17:51 -0500

    Add debug logging to initialization function

--- register.go
+++ register.go
@@ -1,10 +1,15 @@
 package kuzu
 
 import (
+	"fmt"
+
 	"github.com/ctrl-schaff/xk6-kuzu/kuzu"
 	"go.k6.io/k6/js/modules"
 )
 
 func init() {
-	modules.Register(kuzu.ImportPath, kuzu.New())
+	fmt.Printf("xk6-kuzu import pathing: %s\n", kuzu.ImportPath)
+	newModule := kuzu.New()
+	fmt.Printf("Module %v", newModule)
+	modules.Register(kuzu.ImportPath, newModule)
 }
